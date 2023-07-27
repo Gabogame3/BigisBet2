885449e09780dc69fdeb2b1c3f890606c5d0329e
Gabogame3 <gabogame3@gmail.com>
2023-07-27 21:18:44 +0700

    Added all files

---
+++ src/js/data/StoreTable.js
@@ -0,0 +1,127 @@
+export let stores = [
+    {
+     "name": "Micro Marketplace",
+     "stock": [
+      {
+       "id": 0,
+       "qty": 10,
+       "price": 10
+      },
+      {
+       "id": 9,
+       "qty": 3,
+       "price": 50
+      },
+      {
+       "id": 10,
+       "qty": 3,
+       "price": 50
+      },
+      {
+       "id": 11,
+       "qty": 3,
+       "price": 50
+      },
+      {
+       "id": 12,
+       "qty": 3,
+       "price": 50
+      },
+      {
+       "id": 13,
+       "qty": 3,
+       "price": 50
+      },
+      {
+       "id": 14,
+       "qty": 3,
+       "price": 50
+      }
+     ]
+    },
+    {
+     "name": "Standard Shopping Center",
+     "stock": [
+      {
+       "id": 8,
+       "qty": 10,
+       "price": 50
+      },
+      {
+       "id": 4,
+       "qty": 3,
+       "price": 100
+      }
+     ]
+    },
+    {
+     "name": "Macro Megamart",
+     "stock": [
+      {
+       "id": 15,
+       "qty": 10,
+       "price": 100
+      },
+      {
+       "id": 1,
+       "qty": 3,
+       "price": 50
+      },
+      {
+       "id": 2,
+       "qty": 3,
+       "price": 100
+      },
+      {
+        "id": 16,
+        "qty": 3,
+        "price": 10000
+       },
+       {
+        "id": 17,
+        "qty": 3,
+        "price": 500000
+       },
+      {
+       "id": 3,
+       "qty": 3,
+       "price": 100
+      },
+      {
+       "id": 5,
+       "qty": 3,
+       "price": 100
+      },
+      {
+        "id": 18,
+        "qty": 3,
+        "price": 10000
+       },
+       {
+        "id": 20,
+        "qty": 3,
+        "price": 500000
+       },
+      {
+       "id": 6,
+       "qty": 3,
+       "price": 100
+      },
+      {
+        "id": 19,
+        "qty": 3,
+        "price": 10000
+       },
+       {
+        "id": 21,
+        "qty": 3,
+        "price": 500000
+       },
+      {
+       "id": 7,
+       "qty": 3,
+       "price": 100
+      }
+     ]
+    }
+   ]
